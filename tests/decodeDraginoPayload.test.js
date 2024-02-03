9e719198e49f1d35f4bec558685bbb984016daa8
Andrea Del Corto <andrea.delcorto@gmail.com>
2024-02-03 12:56:24 +0100

    formatting

--- tests/decodeDraginoPayload.test.js
+++ tests/decodeDraginoPayload.test.js
@@ -3,7 +3,11 @@ import {hexStringToByteArray} from "../src/hexStringToByteArray.mjs";
 
 describe('decodeDraginoPayload', () => {
     test('Decode complete example', () => {
-        expect(decodeDraginoPayload(hexStringToByteArray("06765FF2960A4B4504D2FB2E"))).toEqual({
+        expect(
+            decodeDraginoPayload(
+                hexStringToByteArray("06765FF2960A4B4504D2FB2E")
+            )
+        ).toEqual({
             "alarm": true,
             "battery": 2.885,
             "latitude": 42.3519,
